4cf4010457e4bdd556ca4da2bfecbe051150be29
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-03-13 01:54:31 +0530

    Delete image.js

--- image.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const imageSchema = new mongoose.Schema({
-        image_date: String,
-        image_url: String,
-        image_uploaded_by: String
-})
-
-module.exports = mongoose.models.Image || mongoose.model('Image', imageSchema);
