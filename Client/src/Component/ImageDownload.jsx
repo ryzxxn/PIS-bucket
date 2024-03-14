c588acc67ee2fe4d38bbd1d0cbdecaa98fe8b3de
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-03-15 01:14:29 +0530

    add link copy

--- Client/src/Component/ImageDownload.jsx
+++ Client/src/Component/ImageDownload.jsx
@@ -1,35 +1,55 @@
-import {useState, useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function ImageDownload() {
-    const [imageUrls, setImageUrls] = useState(null)
-
+    const [imageUrls, setImageUrls] = useState(null);
 
     useEffect(() => {
         // Fetch image URLs from the server
         axios.get('https://upload-io.onrender.com/images')
-          .then(response => {
-            setImageUrls(response.data);
-          })
-          .catch(error => {
-            console.error('Error fetching image URLs:', error.message);
-          });
-      }, []);
-  return (
-    <>
-    <div>
-        <h3>Images</h3>
-    {imageUrls &&(
-    <>
-    <h2>{imageUrls.length}</h2>
-    <div className='list_container'>
-    {imageUrls.map((url, index) => (
-        <img className='list_element' key={index} src={url.url} alt={`Image ${index}`} style={{ maxWidth: '300px', marginTop: '0px' }} >
-        </img>
-    ))}
-    </div>
-    </>)}
-    </div>
-</>
-  )
+            .then(response => {
+                setImageUrls(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching image URLs:', error.message);
+            });
+    }, []);
+
+    const copyImageLink = (imageUrl) => {
+        // Create a temporary textarea element
+        const textarea = document.createElement('textarea');
+        textarea.value = imageUrl;
+        document.body.appendChild(textarea);
+        textarea.select();
+
+        // Copy the URL to the clipboard
+        document.execCommand('copy');
+
+        // Remove the textarea from the DOM
+        document.body.removeChild(textarea);
+    };
+
+    return (
+        <div>
+            <h3>Images</h3>
+            {imageUrls && (
+                <>
+                    <h2>{imageUrls.length}</h2>
+                    <div className='list_container'>
+                        {imageUrls.map((url, index) => (
+                            <div key={index} className='image_container'>
+                                <img
+                                    className='list_element'
+                                    src={url.url}
+                                    alt={`Image ${index}`}
+                                    style={{ maxWidth: '300px', marginTop: '10px' }}
+                                />
+                                <button onClick={() => copyImageLink(url.url)}>Copy Link</button>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
+    );
 }
